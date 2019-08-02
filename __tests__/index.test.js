86e22019a5dd9f4ffe5b367d1b02dcdd160e7d98
Max Perepelyuk <maksim.perepelyuk@mail.ru>
2019-08-02 20:28:07 +0300

    add compare ini format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,6 +5,8 @@ const pathToFileJson1 = '__tests__/__fixtures__/before1.json';
 const pathToFileJson2 = '__tests__/__fixtures__/after1.json';
 const pathToFileYaml1 = '__tests__/__fixtures__/before.yml';
 const pathToFileYaml2 = '__tests__/__fixtures__/after.yml';
+const pathToFileIni1 = '__tests__/__fixtures__/before.ini';
+const pathToFileIni2 = '__tests__/__fixtures__/after.ini';
 fs.writeFileSync('__tests__/__fixtures__/correct-output1', gendiff(pathToFileJson1, pathToFileJson2));
 const correctOutput = fs.readFileSync('__tests__/__fixtures__/correct-output1', 'utf-8');
 
@@ -14,4 +16,8 @@ test('compare format .json', () => {
 
 test('compare format .yml', () => {
   expect(gendiff(pathToFileYaml1, pathToFileYaml2)).toBe(correctOutput);
+});
+
+test('compare format .ini', () => {
+  expect(gendiff(pathToFileIni1, pathToFileIni2)).toBe(correctOutput);
 });
--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,12 @@
 import path from 'path';
 import fs from 'fs';
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parsers = {
   json: JSON.parse,
   yml: yaml.safeLoad,
+  ini: ini.parse,
 };
 
 const getParser = type => parsers[type];
@@ -12,7 +14,7 @@ const getParser = type => parsers[type];
 export default (pathToFile1, pathToFile2) => {
   const formatFiles = path.extname(pathToFile1).slice(1);
   const parse = getParser(formatFiles);
-  const parseFile1 = parse(fs.readFileSync(pathToFile1));
-  const parseFile2 = parse(fs.readFileSync(pathToFile2));
+  const parseFile1 = parse(fs.readFileSync(pathToFile1, 'utf-8'));
+  const parseFile2 = parse(fs.readFileSync(pathToFile2, 'utf-8'));
   return { parseFile1, parseFile2 };
 };
