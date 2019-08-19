f09e9389326181045ccab9c005f91c9c111df29e
Max Perepelyuk <maksim.perepelyuk@mail.ru>
2019-08-19 16:26:46 +0300

    update parsers module

--- src/parsers.js
+++ src/parsers.js
@@ -9,10 +9,17 @@ const parsers = {
   ini: ini.parse,
 };
 
+const getContent = (pathToFileBefore, pathToFileAfter) => {
+  const contentFileBefore = fs.readFileSync(pathToFileBefore, 'utf-8');
+  const contentFileAfter = fs.readFileSync(pathToFileAfter, 'utf-8');
+  return { contentFileBefore, contentFileAfter };
+};
+
 export default (pathToFileBefore, pathToFileAfter) => {
+  const { contentFileBefore, contentFileAfter } = getContent(pathToFileBefore, pathToFileAfter);
   const formatFile = path.extname(pathToFileBefore).slice(1);
   const parse = parsers[formatFile];
-  const parsedFileBefore = parse(fs.readFileSync(pathToFileBefore, 'utf-8'));
-  const parsedFileAfter = parse(fs.readFileSync(pathToFileAfter, 'utf-8'));
+  const parsedFileBefore = parse(contentFileBefore);
+  const parsedFileAfter = parse(contentFileAfter);
   return { parsedFileBefore, parsedFileAfter };
-};
+}
